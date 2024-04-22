e764687182f75aba685b43267d0b530da6fea859
drWali <100865360+drWali@users.noreply.github.com>
2024-04-22 18:45:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
 from setuptools import setup
-
 setup(
-
     name=”my_package”,
-
     version=”0.1″,
-
     description=”A simple Python package for adding numbers.”,
-
     author=”tursunwali”,
-
     author_email=”tursun@gnowit.com”,
-
     url=”https://github.com/drWali/my_package”,
-
     packages=[“my_package”],
-
 )
